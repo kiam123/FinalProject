e2a06f736d4813220c368f78cf3475577f1589f8
kiam <kiam04@hotmail.com>
2017-04-26 03:53:30 +0800

    no message

--- app/src/main/java/tw/edu/fcu/recommendedfood/Activity/ArticleBlogActivity.java
+++ app/src/main/java/tw/edu/fcu/recommendedfood/Activity/ArticleBlogActivity.java
@@ -28,10 +28,47 @@ public class ArticleBlogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_article_blog);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-        html =    "<h1>this is h1</h1>"
-                + "<p>This text is normal</p>"
-                + "<a href=\"http://i.imgur.com/RyFb0yU.jpg\">http://i.imgur.com/RyFb0yU.jpg</a> "
-                + "<img src='http://i.imgur.com/RyFb0yU.jpg' />";
+//        html =    "<h1>this is h1</h1>"
+//                + "<p>This text is normal</p>"
+//                + "<a href=\"http://i.imgur.com/RyFb0yU.jpg\">http://i.imgur.com/RyFb0yU.jpg</a> "
+//                + "<img src='http://i.imgur.com/RyFb0yU.jpg' />";
+
+        html =  "<h1>表特首po，手機排版傷眼見諒。</h1>" +
+                "<p>本魯的好友，一開始認識就覺得很正，但是又不知道正在哪，所以歸類為樸素系的正妹。</p>" +
+                "<p>可以不愛，但請別傷害。<p>" +
+                "<a href=\"http://i.imgur.com/WBpYpsI.jpg\">http://i.imgur.com/WBpYpsI.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/WBpYpsI.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/5zYT6cb.jpg\">http://i.imgur.com/5zYT6cb.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/5zYT6cb.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/LPeOlhE.jpg\">http://i.imgur.com/LPeOlhE.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/LPeOlhE.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/0IN4YQw.jpg\">http://i.imgur.com/0IN4YQw.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/0IN4YQw.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/WRklUTv.jpg\">http://i.imgur.com/WRklUTv.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/WRklUTv.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/blzol5m.jpg\">http://i.imgur.com/blzol5m.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/blzol5m.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/DgsxKf5.jpg\">http://i.imgur.com/DgsxKf5.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/DgsxKf5.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/Bus1uwx.jpg\">http://i.imgur.com/Bus1uwx.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/Bus1uwx.jpg' /><br><br/>"+
+                "<a href=\"http://i.imgur.com/XOdIVZB.jpg\">http://i.imgur.com/XOdIVZB.jpg</a><br><br/>" +
+                "<img src='http://i.imgur.com/XOdIVZB.jpg' /><br><br/>";
+
+//        html =    "表特首po，手機排版傷眼見諒。\n" +
+//                "本魯的好友，一開始認識就覺得很正，但是又不知道正在哪，所以歸類為樸素系的正妹。\n" +
+//                "\n" +
+//                "可以不愛，但請別傷害。\n" +
+//                "\n" +
+//                "http://i.imgur.com/WBpYpsI.jpg\n" +
+//                "http://i.imgur.com/5zYT6cb.jpg\n" +
+//                "http://i.imgur.com/LPeOlhE.jpg\n" +
+//                "http://i.imgur.com/0IN4YQw.jpg\n" +
+//                "http://i.imgur.com/WRklUTv.jpg\n" +
+//                "http://i.imgur.com/blzol5m.jpg\n" +
+//                "http://i.imgur.com/DgsxKf5.jpg\n" +
+//                "http://i.imgur.com/Bus1uwx.jpg\n" +
+//                "http://i.imgur.com/XOdIVZB.jpg";
 
         this.textView = (TextView)this.findViewById(R.id.txt_blog_content);
         PicassoImageGetter picassoImageGetter = new PicassoImageGetter(textView,imageView.getResources(), Picasso.with(this));
