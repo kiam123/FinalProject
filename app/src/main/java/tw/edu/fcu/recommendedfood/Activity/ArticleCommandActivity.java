718ff84db45b7c9a471bd5ba5cd4bcb8e6ae8986
kiam <kiam04@hotmail.com>
2017-05-10 01:21:26 +0800

    can upload photo and take photo

--- app/src/main/java/tw/edu/fcu/recommendedfood/Activity/ArticleCommandActivity.java
+++ app/src/main/java/tw/edu/fcu/recommendedfood/Activity/ArticleCommandActivity.java
@@ -1,7 +1,14 @@
 package tw.edu.fcu.recommendedfood.Activity;
 
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -11,14 +18,29 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import tw.edu.fcu.recommendedfood.R;
+import tw.edu.fcu.recommendedfood.Widget.RichTextEditor;
 
 public class ArticleCommandActivity extends AppCompatActivity {
     private EditText edtTitle;
-    private EditText edtMsg;
+    private RichTextEditor edtMsg;
     private String arr[];
+//    private ImageView imgEditorGallery;
     String title;
     String msg = "";
+
+    private static final int REQUEST_CODE_PICK_IMAGE = 1023;
+    private static final int REQUEST_CODE_CAPTURE_CAMEIA = 1022;
+    private static final File PHOTO_DIR = new File(
+            Environment.getExternalStorageDirectory() + "/DCIM/Camera");
+    private File mCurrentPhotoFile;// 照相机拍照得到的图片
+    String imageEncoded;
+    List<String> imagesEncodedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +50,13 @@ public class ArticleCommandActivity extends AppCompatActivity {
         initView();
     }
 
-
     //TODO 之後發文的時候會使用setOnEditorActionListener
     public void initView() {
         edtTitle = (EditText) findViewById(R.id.edt_title);
-        edtMsg = (EditText) findViewById(R.id.edt_msg);
-//        edtMsg.addTextChangedListener(new NewTextWatcher(edtMsg));
-        edtMsg.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+        edtMsg = (RichTextEditor) findViewById(R.id.edt_msg);
+//        imgEditorGallery = (ImageView) findViewById(R.id.img_editor_gallery);
+
+        edtMsg.firstEdit.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (event == null) {
@@ -59,7 +81,7 @@ public class ArticleCommandActivity extends AppCompatActivity {
                         // We capture the event when the key is first pressed.
                         StringBuffer tempMsg = new StringBuffer(v.getText().toString());
                         tempMsg.append("\n");
-                        Log.v("tempMsg",tempMsg.toString());
+                        Log.v("tempMsg", tempMsg.toString());
 
                         arr = tempMsg.toString().split("\n");
                         for (int i = 0; i < arr.length; i++) {
@@ -68,7 +90,7 @@ public class ArticleCommandActivity extends AppCompatActivity {
 
                         msg = tempMsg.toString();
                         v.setText(msg);
-                        edtMsg.setSelection(msg.length());
+                        edtMsg.firstEdit.setSelection(msg.length());
                     } else {
                         // We consume the event when the key is released.
                         return true;
@@ -88,20 +110,96 @@ public class ArticleCommandActivity extends AppCompatActivity {
 //        finish();
     }
 
-    public void commitMsg(){
+    public void commitMsg() {
         title = "<p>" + edtTitle.getText().toString() + "</p>";
-        msg = edtMsg.getText().toString()+"\n";
-        arr = msg.split("\n ");
+//        msg = edtMsg.getText().toString() + "\n";
+        arr = msg.split("\n");
         Log.v("testing", title);
 
-        if (arr != null && !arr[0].equals("")) {
-            for (int i = 0; i < arr.length; i++) {
-                Log.v("testing", "<p>" + arr[i] + "</p><br>");
-            }
-        } else {
-            msg = "<p>" + edtMsg.getText().toString() + "</p>";
-            Log.v("testing", msg);
+        for (int i = 0; i < arr.length; i++) {
+            Log.v("testing", "<p>" + arr[i] + "</p><br>");
         }
+    }
+
+    public void OnGalleryClick(View view){
+        if(view.getId() == R.id.img_editor_gallery) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");// 相片类型
+            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+        }else if(view.getId() == R.id.img_camera){
+            openCamera();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            Uri uri = data.getData();
+            insertBitmap(getRealFilePath(uri));
+        } else if (requestCode == REQUEST_CODE_CAPTURE_CAMEIA) {
+            insertBitmap(mCurrentPhotoFile.getAbsolutePath());
+        }
+    }
+    private void insertBitmap(String imagePath) {
+        edtMsg.insertImage(imagePath);
+    }
+
+    protected void openCamera() {
+        try {
+            // Launch camera to take photo for selected contact
+            PHOTO_DIR.mkdirs();// 创建照片的存储目录
+            mCurrentPhotoFile = new File(PHOTO_DIR, getPhotoFileName());// 给新照的照片文件命名
+            final Intent intent = getTakePickIntent(mCurrentPhotoFile);
+            startActivityForResult(intent, REQUEST_CODE_CAPTURE_CAMEIA);
+        } catch (ActivityNotFoundException e) {
+        }
+    }
+
+    /**
+     * 用当前时间给取得的图片命名
+     */
+    private String getPhotoFileName() {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "'IMG'_yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(date) + ".jpg";
+    }
+
+    public static Intent getTakePickIntent(File f) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE, null);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        return intent;
+    }
+
+        public String getRealFilePath(final Uri uri) {
+        if (null == uri) {
+            return null;
+        }
+
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = getContentResolver().query(uri,
+                    new String[] { MediaStore.Images.ImageColumns.DATA }, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
     }
 
     public void imgBackAtivity(View view) {
